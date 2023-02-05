9ac43715108f051d019cf9b7896240f263628328
DELL <snehal18122004@gmail.com>
2023-02-05 10:55:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:layout_2/second.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: second(),
+    ),
+  );
+}
